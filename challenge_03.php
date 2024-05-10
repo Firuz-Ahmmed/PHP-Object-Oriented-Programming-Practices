04d8b8108d67257616689548f12f5b324d0ff9f2
Firuz Ahmmed <iamfiruz70@gmail.com>
2024-05-10 23:15:54 +0600

    Challenge03

--- challenge_03.php
+++ challenge_03.php
@@ -1,71 +1,71 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>My Website</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-    <?php 
-     // Define a class called "bicycle"
+<?php 
+     // Define a class called "Bicycle"
      class Bicycle{
+        // Declare public properties for the Bicycle class
         public $brand;
         public $model;
         public $year;
         public $description;
+        // Declare private property for the Bicycle class
         private $weight_kg=0.0;
+        // Declare protected property for the Bicycle class
         protected $wheels=2;
      
-        // Define a method to get the name of the bicycle
+        // Method to return the name of the bicycle
         public function name(){
             return $this->brand." ". $this->model." "."(". $this->year.")";
         }
+        // Method to return the wheel details of the bicycle
         public function wheel_details(){
             $wheel_string=$this->wheels==1?"1 wheel":"{$this->wheels} wheels";
             return "It has ".$wheel_string;
         }        
-        // Define a method to convert weight from kg to lbs
+        // Method to return the weight of the bicycle in kg
         public function weight_kg(){
             return $this->weight_kg ."kg";
         }
+        // Method to return the weight of the bicycle in lbs
         public function weight_lbs(){
             return $this->weight_kg * 2.2046226218 ."lbs";
         }
 
-        
-        // Define a method to set the weight of the bicycle in lbs
+        // Method to set the weight of the bicycle in kg
         public function set_weight_kg($value){
             $this->weight_kg = floatval($value);
         }
     }
+    // Define a class called "Unicycle" which extends the "Bicycle" class
     class Unicycle extends Bicycle{
+        // Override the $wheels property for the Unicycle class
         protected $wheels=1;
     }
     
-    // Create an instance of the bicycle class
+    // Create an instance of the Bicycle class
     $bicycle1 = new Bicycle;
     
-    // Set the properties of the bicycle
+    // Set the properties of the Bicycle instance
     $bicycle1->brand="Toyota";
     $bicycle1->model="Corolla";
     $bicycle1->year=2020;
 
+    // Create an instance of the Unicycle class
     $uni= new Unicycle;
+    // Display the wheel details of the Bicycle and Unicycle instances
     echo "Bicycle: ".$bicycle1->wheel_details()."<br/>";
     echo "Unicycle: ".$uni->wheel_details()."<br/>";
     echo "<hr/>";
+    // Set the weight of the Bicycle instance using kg
     echo "Set weight using kg<br/>";
     $bicycle1->set_weight_kg(1000);
-    // Output the name of the bicycle
+    // Display the weight of the Bicycle instance in kg and lbs
     echo $bicycle1->weight_kg()."<br/>";
     echo $bicycle1->weight_lbs()."<br/>";
     echo "<hr/>";
+    // Set the weight of the Bicycle instance using lbs
     echo "Set weight using lbs<br/>";
     $bicycle1->set_weight_kg(2000);
-    // Output the name of the bicycle
+    // Display the weight of the Bicycle instance in kg and lbs
     echo $bicycle1->weight_kg()."<br/>";
     echo $bicycle1->weight_lbs()."<br/>";
     echo "<hr/>";
     
-     ?>
-</body>
-</html>
